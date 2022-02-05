a774d93bb6d199fe6bc64d9964fbbc64ad947f75
jk-gan <junkai@hey.com>
2022-02-05 20:46:15 +0800

    feat: shading with surface normal

--- src/main.rs
+++ src/main.rs
@@ -11,20 +11,28 @@ const ASPECT_RATIO: f32 = 16.0 / 9.0;
 const WIDTH: u32 = 400;
 const HEIGHT: u32 = (WIDTH as f32 / ASPECT_RATIO) as u32;
 
-fn hit_sphere(center: &Point3, radius: f64, r: &Ray) -> bool {
+fn hit_sphere(center: &Point3, radius: f64, r: &Ray) -> f64 {
     let oc = r.origin().clone() - center.clone();
     let a = r.direction().dot(*r.direction());
     let b = 2.0 * oc.dot(*r.direction());
     let c = oc.dot(oc) - radius * radius;
     let discriminant = b * b - 4.0 * a * c;
 
-    discriminant > 0.0
+    if discriminant < 0.0 {
+        return -1.0;
+    } else {
+        return (-b - discriminant.sqrt()) / (2.0 * a);
+    }
 }
 
 fn ray_color(r: &Ray) -> Color {
-    if hit_sphere(&Point3::new(0.0, 0.0, -1.0), 0.5, r) {
-        return Color::new(1.0, 0.0, 0.0);
+    let t = hit_sphere(&Point3::new(0.0, 0.0, -1.0), 0.5, r);
+
+    if t > 0.0 {
+        let N = (r.at(t) - DVec3::new(0.0, 0.0, -1.0)).normalize();
+        return 0.5 * Color::new(N.x + 1.0, N.y + 1.0, N.z + 1.0);
     }
+
     let unit_direction = r.direction().normalize();
     let t = 0.5 * (unit_direction.y + 1.0);
     // lerp
